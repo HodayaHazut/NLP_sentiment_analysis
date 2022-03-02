42bab02bd99d19642fc4a82d4086624e3fcc3db3
HodayaHazut <85248495+HodayaHazut@users.noreply.github.com>
2022-03-02 11:18:20 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+hodaya749, Hodaya Hazut, 207876749
+
+Ex 3 - Files:
+-------------
+* README
+* ex3.py - Models implementation and calculation of accuracy and loss values.
+* Answers.pdf - Answers to the theoretical questions.
---
+++ ex3.py
@@ -0,0 +1,504 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+import os
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+import operator
+import data_loader
+import pickle
+import tqdm
+import matplotlib.pyplot as plt
+
+# ------------------------------------------- Constants ----------------------------------------
+SEQ_LEN = 52
+W2V_EMBEDDING_DIM = 300
+
+ONEHOT_AVERAGE = "onehot_average"
+W2V_AVERAGE = "w2v_average"
+W2V_SEQUENCE = "w2v_sequence"
+
+TRAIN = "train"
+VAL = "val"
+TEST = "test"
+RARE_WORD = "rare_words"
+NEG_POLARITY = "negated_polarity"
+
+
+# ------------------------------------------ Helper methods and classes --------------------------
+
+def get_available_device():
+    """
+    Allows training on GPU if available. Can help with running things faster when a GPU with cuda is
+    available but not a most...
+    Given a device, one can use module.to(device)
+    and criterion.to(device) so that all the computations will be done on the GPU.
+    """
+    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def save_pickle(obj, path):
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def load_pickle(path):
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
+
+def save_model(model, path, epoch, optimizer):
+    """
+    Utility function for saving checkpoint of a model, so training or evaluation can be executed later on.
+    :param model: torch module representing the model
+    :param optimizer: torch optimizer used for training the module
+    :param path: path to save the checkpoint into
+    """
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict()}, path)
+
+
+def load(model, path, optimizer):
+    """
+    Loads the state (weights, paramters...) of a model which was saved with save_model
+    :param model: should be the same model as the one which was saved in the path
+    :param path: path to the saved checkpoint
+    :param optimizer: should be the same optimizer as the one which was saved in the path
+    """
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    return model, optimizer, epoch
+
+
+# ------------------------------------------ Data utilities ----------------------------------------
+
+def load_word2vec():
+    """ Load Word2Vec Vectors
+        Return:
+            wv_from_bin: All 3 million embeddings, each lengh 300
+    """
+    import gensim.downloader as api
+    wv_from_bin = api.load("word2vec-google-news-300")
+    vocab = list(wv_from_bin.vocab.keys())
+    print(wv_from_bin.vocab[vocab[0]])
+    print("Loaded vocab size %i" % len(vocab))
+    return wv_from_bin
+
+
+def create_or_load_slim_w2v(words_list, cache_w2v=False):
+    """
+    returns word2vec dict only for words which appear in the dataset.
+    :param words_list: list of words to use for the w2v dict
+    :param cache_w2v: whether to save locally the small w2v dictionary
+    :return: dictionary which maps the known words to their vectors
+    """
+    w2v_path = "w2v_dict.pkl"
+    if not os.path.exists(w2v_path):
+        full_w2v = load_word2vec()
+        w2v_emb_dict = {k: full_w2v[k] for k in words_list if k in full_w2v}
+        if cache_w2v:
+            save_pickle(w2v_emb_dict, w2v_path)
+    else:
+        w2v_emb_dict = load_pickle(w2v_path)
+    return w2v_emb_dict
+
+
+def get_w2v_average(sent, word_to_vec, embedding_dim):
+    """
+    This method gets a sentence and returns the average word embedding of the words consisting
+    the sentence.
+    :param sent: the sentence object
+    :param word_to_vec: a dictionary mapping words to their vector embeddings
+    :param embedding_dim: the dimension of the word embedding vectors
+    :return The average embedding vector as numpy ndarray.
+    """
+    w2v_embedding = np.zeros(embedding_dim)
+    for word in sent.text:
+        if word in word_to_vec:
+            w2v_embedding += word_to_vec[word]
+    return w2v_embedding / len(sent.text)
+
+
+def get_one_hot(size, ind):
+    """
+    this method returns a one-hot vector of the given size, where the 1 is placed in the ind entry.
+    :param size: the size of the vector
+    :param ind: the entry index to turn to 1
+    :return: numpy ndarray which represents the one-hot vector
+    """
+    one_hot_vector = np.zeros(size)
+    one_hot_vector[ind] = 1
+    return one_hot_vector
+
+
+def average_one_hots(sent, word_to_ind):
+    """
+    this method gets a sentence, and a mapping between words to indices, and returns the average
+    one-hot embedding of the tokens in the sentence.
+    :param sent: a sentence object.
+    :param word_to_ind: a mapping between words to indices
+    :return:
+    """
+    one_hot_num = 0
+    size = len(word_to_ind.keys())
+    one_hot_embedding = np.zeros((size,))
+    for word in sent.text:
+        index = word_to_ind[word]
+        one_hot_vec = get_one_hot(size, index)
+        one_hot_num += 1
+        one_hot_embedding += one_hot_vec
+    return one_hot_embedding / one_hot_num
+
+
+def get_word_to_ind(words_list):
+    """
+    this function gets a list of words, and returns a mapping between
+    words to their index.
+    :param words_list: a list of words
+    :return: the dictionary mapping words to the index
+    """
+    dictionary_mapping = {}
+    for i in range(len(words_list)):
+        word = words_list[i]
+        if word not in dictionary_mapping:
+            dictionary_mapping[word] = i
+    return dictionary_mapping
+
+
+def sentence_to_embedding(sent, word_to_vec, seq_len, embedding_dim=300):
+    """
+    this method gets a sentence and a word to vector mapping, and returns a list containing the
+    words embeddings of the tokens in the sentence.
+    :param sent: a sentence object
+    :param word_to_vec: a word to vector mapping.
+    :param seq_len: the fixed length for which the sentence will be mapped to.
+    :param embedding_dim: the dimension of the w2v embedding
+    :return: numpy ndarray of shape (seq_len, embedding_dim) with the representation of the sentence
+    """
+    sent_embedding = np.zeros((seq_len, embedding_dim))
+    for i, word in enumerate(sent.text):
+        if i < seq_len:
+            if word in word_to_vec:
+                sent_embedding[i] = word_to_vec[word]
+            else:
+                sent_embedding[i] = np.zeros(embedding_dim)
+    return sent_embedding
+
+
+class OnlineDataset(Dataset):
+    """
+    A pytorch dataset which generates model inputs on the fly from sentences of SentimentTreeBank
+    """
+
+    def __init__(self, sent_data, sent_func, sent_func_kwargs):
+        """
+        :param sent_data: list of sentences from SentimentTreeBank
+        :param sent_func: Function which converts a sentence to an input datapoint
+        :param sent_func_kwargs: fixed keyword arguments for the state_func
+        """
+        self.data = sent_data
+        self.sent_func = sent_func
+        self.sent_func_kwargs = sent_func_kwargs
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sent = self.data[idx]
+        sent_emb = self.sent_func(sent, **self.sent_func_kwargs)
+        sent_label = sent.sentiment_class
+        return sent_emb, sent_label
+
+
+class DataManager():
+    """
+    Utility class for handling all data management task. Can be used to get iterators for training and
+    evaluation.
+    """
+
+    def __init__(self, data_type=ONEHOT_AVERAGE, use_sub_phrases=True, dataset_path="stanfordSentimentTreebank",
+                 batch_size=50,
+                 embedding_dim=None):
+        """
+        builds the data manager used for training and evaluation.
+        :param data_type: one of ONEHOT_AVERAGE, W2V_AVERAGE and W2V_SEQUENCE
+        :param use_sub_phrases: if true, training data will include all sub-phrases plus the full sentences
+        :param dataset_path: path to the dataset directory
+        :param batch_size: number of examples per batch
+        :param embedding_dim: relevant only for the W2V data types.
+        """
+
+        # load the dataset
+        self.sentiment_dataset = SentimentTreeBank(dataset_path, split_words=True)
+        # map data splits to sentences lists
+        self.sentences = {}
+        if use_sub_phrases:
+            self.sentences[TRAIN] = self.sentiment_dataset.get_train_set_phrases()
+        else:
+            self.sentences[TRAIN] = self.sentiment_dataset.get_train_set()
+
+        self.sentences[VAL] = self.sentiment_dataset.get_validation_set()
+        self.sentences[TEST] = self.sentiment_dataset.get_test_set()
+        self.sentences[RARE_WORD] = \
+            [self.sentiment_dataset.get_test_set()[i]
+             for i in get_negated_polarity_examples(self.sentiment_dataset.get_test_set())]
+        self.sentences[NEG_POLARITY] = \
+            [self.sentiment_dataset.get_test_set()[i]
+             for i in get_rare_words_examples(self.sentiment_dataset.get_test_set(), self.sentiment_dataset)]
+
+        # map data splits to sentence input preperation functions
+        words_list = list(self.sentiment_dataset.get_word_counts().keys())
+        if data_type == ONEHOT_AVERAGE:
+            self.sent_func = average_one_hots
+            self.sent_func_kwargs = {"word_to_ind": get_word_to_ind(words_list)}
+        elif data_type == W2V_SEQUENCE:
+            self.sent_func = sentence_to_embedding
+
+            self.sent_func_kwargs = {"seq_len": SEQ_LEN,
+                                     "word_to_vec": create_or_load_slim_w2v(words_list),
+                                     "embedding_dim": embedding_dim
+                                     }
+        elif data_type == W2V_AVERAGE:
+            self.sent_func = get_w2v_average
+            words_list = list(self.sentiment_dataset.get_word_counts().keys())
+            self.sent_func_kwargs = {"word_to_vec": create_or_load_slim_w2v(words_list),
+                                     "embedding_dim": embedding_dim
+                                     }
+        else:
+            raise ValueError("invalid data_type: {}".format(data_type))
+        # map data splits to torch datasets and iterators
+        self.torch_datasets = {k: OnlineDataset(sentences, self.sent_func, self.sent_func_kwargs) for
+                               k, sentences in self.sentences.items()}
+        self.torch_iterators = {k: DataLoader(dataset, batch_size=batch_size, shuffle=k == TRAIN)
+                                for k, dataset in self.torch_datasets.items()}
+
+    def get_torch_iterator(self, data_subset=TRAIN):
+        """
+        :param data_subset: one of TRAIN VAL and TEST
+        :return: torch batches iterator for this part of the datset
+        """
+        return self.torch_iterators[data_subset]
+
+    def get_labels(self, data_subset=TRAIN):
+        """
+        :param data_subset: one of TRAIN VAL and TEST
+        :return: numpy array with the labels of the requested part of the datset in the same order of the
+        examples.
+        """
+        return np.array([sent.sentiment_class for sent in self.sentences[data_subset]])
+
+    def get_input_shape(self):
+        """
+        :return: the shape of a single example from this dataset (only of x, ignoring y the label).
+        """
+        return self.torch_datasets[TRAIN][0][0].shape
+
+
+# ------------------------------------ Models ----------------------------------------------------
+
+class LSTM(nn.Module):
+    """
+    An LSTM for sentiment analysis with architecture as described in the exercise description.
+    """
+
+    def __init__(self, embedding_dim, hidden_dim, n_layers, dropout):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.biLSTM = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, num_layers=n_layers, dropout=dropout,
+                              bidirectional=True, batch_first=True)
+        self.linear_out = nn.Linear(in_features=hidden_dim * 2, out_features=1)
+
+    def forward(self, text):
+        out_lstm, (h_n, c_n) = self.biLSTM(text)
+        out_reversed = out_lstm[:, 0, self.hidden_dim:]
+        out_direct = out_lstm[:, -1, :self.hidden_dim]
+        out_final_lstm = torch.cat((out_reversed, out_direct), 1)
+        return self.linear_out(out_final_lstm)
+
+    def predict(self, text):
+        return nn.Sigmoid()(self.forward(text))
+
+
+class LogLinear(nn.Module):
+    """
+    general class for the log-linear models for sentiment analysis.
+    """
+
+    def __init__(self, embedding_dim):
+        super().__init__()
+        self.linearLayer = nn.Linear(in_features=embedding_dim, out_features=1)
+
+    def forward(self, x):
+        return self.linearLayer(x)
+
+    def predict(self, x):
+        return nn.Sigmoid()(self.forward(x))
+
+
+# ------------------------- training functions -------------
+
+
+def binary_accuracy(preds, y):
+    """
+    This method returns tha accuracy of the predictions, relative to the labels.
+    You can choose whether to use numpy arrays or tensors here.
+    :param preds: a vector of predictions
+    :param y: a vector of true labels
+    :return: scalar value - (<number of accurate predictions> / <number of examples>)
+    """
+    num_examples = len(y)
+    num_acc_predict = 0
+    for i in range(num_examples):
+        predict = preds[i]
+        if predict >= 0.5:
+            predict = 1
+        else:
+            predict = 0
+        if predict == y[i]:
+            num_acc_predict += 1
+    return num_acc_predict / num_examples
+
+
+def train_epoch(model, data_iterator, optimizer, criterion):
+    """
+    This method operates one epoch (pass over the whole train set) of training of the given model,
+    and returns the accuracy and loss for this epoch
+    :param model: the model we're currently training
+    :param data_iterator: an iterator, iterating over the training data for the model.
+    :param optimizer: the optimizer object for the training process.
+    :param criterion: the criterion object for the training process.
+    """
+    accuracy, loss = 0, 0
+    for x, y in data_iterator:
+        optimizer.zero_grad()
+        pred = model(x.float()).reshape(y.shape)
+        loss = criterion(pred, y)
+        loss.backward()
+        optimizer.step()
+        accuracy = binary_accuracy(model.predict(x.float()), y)
+    return accuracy, loss.item()
+
+
+def evaluate(model, data_iterator, criterion):
+    """
+    evaluate the model performance on the given data
+    :param model: one of our models..
+    :param data_iterator: torch data iterator for the relevant subset
+    :param criterion: the loss criterion used for evaluation
+    :return: tuple of (average loss over all examples, average accuracy over all examples)
+    """
+    accuracy, loss = 0, 0
+    pred_lst = get_predictions_for_data(model, data_iterator)
+    for x, y in data_iterator:
+        loss = criterion(model(x.float()).reshape(y.shape), y)
+        accuracy = binary_accuracy(model.predict(x.float()), y)
+
+    return loss.item() / len(pred_lst), accuracy / len(pred_lst)
+
+
+def get_predictions_for_data(model, data_iter):
+    """
+
+    This function should iterate over all batches of examples from data_iter and return all of the models
+    predictions as a numpy ndarray or torch tensor (or list if you prefer). the prediction should be in the
+    same order of the examples returned by data_iter.
+    :param model: one of the models you implemented in the exercise
+    :param data_iter: torch iterator as given by the DataManager
+    :return:
+    """
+    pred_lst = []
+    for x, y in data_iter:
+        pred_lst.append(model.predict(x.float()))
+    return pred_lst
+
+
+def train_model(model, data_manager, n_epochs, lr, weight_decay=0.):
+    """
+    Runs the full training procedure for the given model. The optimization should be done using the Adam
+    optimizer with all parameters but learning rate and weight decay set to default.
+    :param model: module of one of the models implemented in the exercise
+    :param data_manager: the DataManager object
+    :param n_epochs: number of times to go over the whole training set
+    :param lr: learning rate to be used for optimization
+    :param weight_decay: parameter for l2 regularization
+    """
+    data_iterator = data_manager.get_torch_iterator(data_subset=TRAIN)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=lr, weight_decay=weight_decay)
+    criterion = nn.BCEWithLogitsLoss()
+
+    test_iterator = data_manager.get_torch_iterator(data_subset=TEST)
+    val_iterator = data_manager.get_torch_iterator(data_subset=VAL)
+
+    train_accuracy_lst, train_loss_lst = [], []
+    val_accuracy_lst, val_loss_lst = [], []
+    for i in range(n_epochs):
+        accuracy, loss = train_epoch(model, data_iterator, optimizer, criterion)
+        accuracy_val, loss_val = evaluate(model, val_iterator, criterion)
+        val_accuracy_lst.append(accuracy_val)
+        val_loss_lst.append(loss_val)
+        train_accuracy_lst.append(accuracy)
+        train_loss_lst.append(loss)
+
+    average_loss, average_accuracy = evaluate(model, test_iterator, criterion)
+    negated_polarity_loss, negated_polarity_acc = evaluate(model,
+                                                           data_manager.get_torch_iterator(data_subset=NEG_POLARITY),
+                                                           criterion)
+    rare_words_loss, rare_words_acc = evaluate(model, data_manager.get_torch_iterator(data_subset=RARE_WORD), criterion)
+
+    print("Test set accuracy is ", average_accuracy)
+    print("Test set loss is ", average_loss)
+    print("Rare words accuracy is ", rare_words_acc)
+    print("Rare words loss is ", rare_words_loss)
+    print("Negated polarity examples accuracy is ", negated_polarity_acc)
+    print("Negated polarity examples loss is ", negated_polarity_loss)
+
+    plt.plot(train_accuracy_lst, color='r', label='Train accuracy')
+    plt.plot(val_accuracy_lst, color='b', label='Validation accuracy')
+    plt.legend()
+    plt.show()
+    plt.plot(train_loss_lst, color='r', label='Train loss')
+    plt.plot(val_loss_lst, color='b', label='Validation loss')
+    plt.legend()
+    plt.show()
+
+
+def train_log_linear_with_one_hot():
+    """
+    Here comes your code for training and evaluation of the log linear model with one hot representation.
+    """
+    print("*****Start train log linear with one hot:*****")
+    data_manager = DataManager(ONEHOT_AVERAGE, batch_size=64)
+    model = LogLinear(data_manager.get_input_shape()[0])
+    train_model(model, data_manager, 20, 0.01, 0.001)
+
+
+def train_log_linear_with_w2v():
+    """
+    Here comes your code for training and evaluation of the log linear model with word embeddings
+    representation.
+    """
+    print("*****Start train log linear with w2v:*****")
+    data_manager = DataManager(W2V_AVERAGE, batch_size=64, embedding_dim=300)
+    model = LogLinear(embedding_dim=300)
+    train_model(model, data_manager, 20, 0.01, 0.001)
+
+
+def train_lstm_with_w2v():
+    """
+    Here comes your code for training and evaluation of the LSTM model.
+    """
+    print("*****Start train lstm with w2v:*****")
+    data_manager = DataManager(W2V_SEQUENCE, batch_size=64, embedding_dim=300)
+    model = LSTM(300, 100, 1, 0.5)
+    train_model(model, data_manager, 4, 0.001, 0.0001)
+
+
+if __name__ == '__main__':
+    train_log_linear_with_one_hot()
+    train_log_linear_with_w2v()
+    train_lstm_with_w2v()
